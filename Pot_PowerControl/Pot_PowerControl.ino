592c364c1d39f18d786c49c8994a8d92bb0d2e96
wondermutt-v1 <adrian.r.perez@outlook.com>
2020-11-03 13:37:49 -0600

    Initial commit

---
+++ Pot_PowerControl/Pot_PowerControl.ino
@@ -0,0 +1,32 @@
+int powerOut(int serIn){
+if (serIn < 255){
+  return serIn;
+  }else return 255;
+} 
+
+void motor1(int digADpin, int digADVal, int anaADpin, int anaADVal){
+  digitalWrite(digADpin, digADVal);
+  analogWrite(anaADpin,anaADVal);
+}
+ 
+ void setup() {
+   Serial.begin(9600);
+   pinMode(4, INPUT);
+   pinMode(5, INPUT);    //Potentiometer input
+   pinMode(7, OUTPUT);   //motor 1 AD bridge
+   pinMode(8, OUTPUT);   //motor 1 AC bridge
+   pinMode(9, OUTPUT);   //motor 1 power
+   }
+
+void loop() {
+  int serIn;
+  int x;
+  int y = digitalRead(4);   //Read in the blue button
+  int potVal = analogRead(5)/2;    // Read in the potentiometer value
+  // put your main code here, to run repeatedly:
+  x = powerOut(potVal);
+  
+  int z = x;
+  motor1(7,1,9,x);
+  Serial.println(x);
+  }
