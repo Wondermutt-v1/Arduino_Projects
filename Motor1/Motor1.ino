06dc6b3bbc7c7e83416bbf52199d5643b1f795bc
Wondermutt-v1 <71840008+Wondermutt-v1@users.noreply.github.com>
2020-11-20 15:48:13 -0600

    Add files via upload

---
+++ Motor1/Motor1.ino
@@ -0,0 +1,40 @@
+//Define pin names
+//#define vMotor
+//#define vMotorA3
+#define bluSwch 4
+#define redTog 3
+
+
+
+//#include <IVT.h>
+
+//IVT ivt;
+//int vMotorA3 = 
+//int vMotor = 
+//const int R1 =
+//const int R2 =
+int analogPin = redTog;
+int digiRead = bluSwch;
+int HOut = 7;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  //ivt.init_IVT();
+  //pinMode( 4,INPUT); //Blue button
+  pinMode(3,OUTPUT);  //Outputs the Red toggle
+  
+  pinMode(9 , OUTPUT);
+  pinMode(7 ,OUTPUT); //Bri AD
+  pinMode(8 ,OUTPUT); // Bri AC
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int Tog = analogRead(analogPin);
+  Serial.println(Tog);
+  digitalWrite(7, 1 );   //activate h-bridge
+  digitalWrite(8, 0);
+  analogWrite( 9 ,120);
+  
+}
